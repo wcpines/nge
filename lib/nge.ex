1b259fd80f4de2af16e43d6cf4753f8792009e2c
Colby Pines <wcpines@gmail.com>
2020-03-02 15:16:51 -0800

    wip

--- lib/nge.ex
+++ lib/nge.ex
@@ -6,18 +6,11 @@ defmodule Nge do
 
   def start do
     children = [
-      {Plug.Cowboy, scheme: :http, plug: Nge.Plug, options: [port: 4001]}
+      worker(Nge.Router, []),
+      worker(Nge.Importer, [])
     ]
 
     opts = [strategy: :one_for_one, name: Nge.Supervisor]
     Supervisor.start_link(children, opts)
-  end
-
-  def run do
-    case ActivityLogParser.parse(@csv) do
-      {:ok, logs} -> Api.post_runs(logs)
-      {:error, msg} -> IO.puts(msg)
-      _ -> IO.puts("borked")
-    end
   end
 end

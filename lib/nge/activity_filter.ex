b0a414a8ac1d05a704f54a5c72cc85faf68c20d3
Colby Pines <wcpines@gmail.com>
2021-03-19 10:36:41 -0700

    chore: add more type specs

--- lib/nge/activity_filter.ex
+++ lib/nge/activity_filter.ex
@@ -1,19 +1,22 @@
 # lib/nge/log_filter.ex
 
 defmodule Nge.ActivityFilter do
-  def new_activities_by_date(strava_logs, new_logs) do
+  @spec new_activities_by_date([Strava.SummaryActivity.t()], [RowLog.t()]) :: [RowLog.t()]
+  def new_activities_by_date(strava_logs, csv_logs) do
     strava_logs
     |> filterable_dates
-    |> filter_duplicates(new_logs)
+    |> filter_duplicates(csv_logs)
   end
 
-  def filter_duplicates(filter_dates, new_logs) do
-    for log <- new_logs,
+  @spec filter_duplicates([DateTime.t()], [RowLog.t()]) :: [RowLog.t()]
+  def filter_duplicates(filter_dates, csv_logs) do
+    for log <- csv_logs,
         log != %{},
         NaiveDateTime.to_date(log.datetime) not in filter_dates,
         do: log
   end
 
+  @spec filterable_dates([Strava.SummaryActivity.t()]) :: [DateTime.t()]
   def filterable_dates(strava_logs) do
     strava_logs
     |> Enum.map(&DateTime.to_date(&1.start_date_local))
--- lib/nge/auth.ex
+++ lib/nge/auth.ex
@@ -2,6 +2,7 @@
 defmodule Nge.Auth do
   alias Strava.Auth, as: Strauth
 
+  @spec gen_client(String.t()) :: Tesla.Env.client()
   def gen_client(code) do
     Strauth.get_token(code: code, grant_type: "authorization_code")
     |> elem(1)
@@ -10,6 +11,7 @@ defmodule Nge.Auth do
     |> Strava.Client.new()
   end
 
+  @spec get_url :: binary
   def get_url do
     Strauth.authorize_url!(
       scope: "profile:write,activity:read_all,activity:write",
